dd9d1b109dc3742c800be86e494e8658825f10bc
F4y03 <natapongnetthip@gmail.com>
2025-02-02 12:20:15 +0700

    Delete my directory

--- my/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // ลบ Session ทั้งหมด
-header("Location: login.php"); // Redirect ไปหน้า Login
-exit();
-?>
--- my/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // ลบ Session ทั้งหมด
-header("Location: index.php"); // Redirect ไปหน้าหลัก
-exit();
-?>
